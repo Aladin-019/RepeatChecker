ae728250d7ecd1485efee55b4001d69ea387ff32
Aladin-019 <aaladino581@gmail.com>
2024-07-31 15:13:24 -0600

    AllWords class tested and working
    Correctly creates 2D array of all words of input document.
    Leading and trailing punctuation is removed.

--- AllWords.java
+++ AllWords.java
@@ -1,36 +1,173 @@
 // Aladin Alihodzic
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * A two-dimensional representation of all words in the document.
- * Each line is a row. AllWords being a list of all rows.
+ * Each line is a row of all words. AllWords being a list of all rows.
  */
-public class AllWords {
+public class AllWords{
 
     /**
      * list of each row in document (containing only words)
      */
-    String[][] All;
+    String[][] All = new String[100][100];
 
     /**
      * position of current word being analyzed in document
      * (so we don't compare current word to itself)
+     * index 0 = row, index 1 = column/word in that row
      */
-    int[] currentPosition;
+    int[] currentPosition = new int[2];
 
     /**
-     * read and parse document and fill All
-     * @param inFile file reader to be analyzed
-     * @return new AllWords object
+     * AllWords simple constructor
+     * current position at first file entry
      */
-    public AllWords initAllWords(BufferedReader inFile){
-        // stub
-        return;
+    public AllWords(String filename){
+        initAll(filename);  // initialize All
+        this.currentPosition = new int[]{0, 0};  // current position at beginning of file
     }
 
-    public static void main(String[] args){
-
+    /**
+     * AllWords constructor
+     * choose current position
+     */
+    public AllWords(String filename, int[] currentPosition){
+        initAll(filename);  // initialize All
+        this.currentPosition = currentPosition;  // set current position
     }
 
+    /**
+     * Removes common punctuation characters found at the beginning of a word
+     * @param word - old word
+     * @return new word
+     */
+    public String removeHead(String word){
+        char firstChar;
+        int len = word.length();
+        // if empty string, return empty string immediately
+        // prevents StringIndexOutOfBoundsException later in the program
+        if (len < 1){
+            return word;
+        }
+
+        int count = 0;
+        firstChar = word.charAt(count);  //current character being checked (starting from front)
+        // while characters left in word and have a match, go to next char
+        while ((firstChar=='.' || firstChar==',' || firstChar==':' || firstChar==';' || firstChar=='{' || firstChar=='['
+                || firstChar=='?' || firstChar=='!'|| firstChar=='(' || firstChar=='<' || firstChar=='-'
+                || firstChar=='\'' || firstChar=='\"') && count < len){
+            count++;
+            firstChar = word.charAt(count);
+        }
+
+        return word.substring(count, len);   // return new string with undesired beginning left out
+    }
+
+    /**
+     * removes common punctuation characters found at the end of a word
+     * @param word - old word
+     * @return new word
+     */
+    public String removeTail(String word){
+        char lastChar;
+        int len = word.length();
+        // if empty string, return empty string immediately
+        // prevents StringIndexOutOfBoundsException later in the program
+        if (len < 1){
+            return word;
+        }
+
+        lastChar = word.charAt(len-1); //current character being checked (starting from back)
+        // while characters left in word and have a match, go to next char
+        while ((lastChar=='.' || lastChar==',' || lastChar==':' || lastChar==';' || lastChar=='?' || lastChar=='!'
+                || lastChar=='}'|| lastChar==']' || lastChar==')' || lastChar=='>' || lastChar=='-'
+                || lastChar=='\'' || lastChar=='\"') && len > 0) {
+
+            //if match and it`s the only character left, return empty string
+            if (len == 1){
+                return "";
+            }
+            len--;
+            lastChar = word.charAt(len - 1);
+        }
+
+        return word.substring(0, len);  // return new string with undesired end left out
+    }
+
+    /**
+     * Read and parse document and fill All (2D array) with only the words of the file
+     * @param filename file to be read
+     */
+    private void initAll(String filename) {
+        BufferedReader fileReader;
+        int wordNum;  // word number of current row (column)
+        try {
+            fileReader = new BufferedReader(new FileReader(filename));
+            String line = fileReader.readLine();   // line of document
+            int lineNum = 0;   // current line number (row)
+            String[] lineArr;  // String Array of line
+
+            // while not at end of file
+            while (line != null) {
+                wordNum = 0;
+                lineArr = line.toLowerCase().split("\\s+");   // parse and clean up
+                // for each line array clean up leading and trailing punctuation and store into
+                // All 2D array.
+                for (int i = 0; i < lineArr.length; i++) {
+                    lineArr[i] = this.removeHead(lineArr[i]);
+                    lineArr[i] = this.removeTail(lineArr[i]);
+                    // only store non-null strings
+                    if (lineArr[i] != null && !Objects.equals(lineArr[i], "")){
+                        All[lineNum][wordNum] = lineArr[i];
+                        wordNum++;
+                    }
+                }
+                lineNum++;
+                line = fileReader.readLine();  //next line
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+        /**
+         * change current position to new position
+         * @param pos new position
+         */
+        public void setCurrentPosition (int[] pos){
+            this.currentPosition = pos;
+        }
+
+        /**
+         * create string of All for testing
+         * @return
+         */
+    public String toString(){
+        String result = "";
+        for (int i=0; i<100; i++){
+            for (int j=0; j<100; j++) {
+                if (All[i][j] != null) {
+                    result = result + " " + All[i][j];
+                }
+            }
+        }
+        return result;
+    }
+
+        // main
+        public static void main (String[] args){
+
+            // test: simple AllWords constructor on text file, display result
+            // (also tests initAll, removeHead, and removeTail in the process)
+            // New test cases added to single testfile.txt document as testing progresses.
+            AllWords words = new AllWords("testfile.txt");
+            String displayAll;
+            displayAll = words.toString();
+            System.out.println(displayAll);
+        }
 }
